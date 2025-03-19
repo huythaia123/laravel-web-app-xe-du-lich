b09244c71470183c54295a8af2aced754065ca23
huy thai <huythaia123@gmail.com>
2025-03-19 10:39:00 +0000

    detached dialog component

---
+++ resources/js/components/BookingInfoDialog/index.tsx
@@ -0,0 +1,159 @@
+import { BookCarInfo } from '@/types';
+import { format } from 'date-fns';
+import { vi } from 'date-fns/locale';
+import { Info } from 'lucide-react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '../ui/alert-dialog';
+import { Button } from '../ui/button';
+
+const formatDate = (dateStr: string) => {
+  const date = new Date(dateStr);
+  return format(date, 'dd/MM/yyyy HH:mm', { locale: vi });
+};
+
+const BookingInfoDialog = ({
+  carBookingInfo,
+}: {
+  carBookingInfo: BookCarInfo;
+}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" className="cursor-pointer">
+          <Info className="sm:hidden" />
+          <p className="hidden sm:block">Xem chi tiết</p>
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent className="max-w-2xl">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Thông tin chi tiết</AlertDialogTitle>
+        </AlertDialogHeader>
+        {/* Bảng hiển thị thông tin */}
+        <div className="overflow-auto">
+          <table className="w-full border-collapse border text-sm">
+            <tbody>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  ID
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {carBookingInfo.id}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Họ tên
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {carBookingInfo.ho_ten}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Điểm đón
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {carBookingInfo.diem_don}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Điểm đến
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {carBookingInfo.diem_den}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Hai chiều
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {carBookingInfo.check_hai_chieu ? 'Có' : 'Không'}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  VAT
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {carBookingInfo.check_vat ? 'Có' : 'Không'}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Loại xe
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {carBookingInfo.loai_xe} Chỗ
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Số điện thoại
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {carBookingInfo.so_dien_thoai}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Thời gian đón
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {formatDate(carBookingInfo.thoi_gian_don)}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Thời gian chờ
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {carBookingInfo.thoi_gian_cho}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Thời gian đặt xe
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {formatDate(carBookingInfo.created_at)}
+                </td>
+              </tr>
+              <tr className="border">
+                <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
+                  Chỉnh sửa lần cuối
+                </td>
+                <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
+                  {formatDate(carBookingInfo.updated_at)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <AlertDialogFooter>
+          <AlertDialogCancel className="cursor-pointer">Đóng</AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button
+              variant={'outline'}
+              className="cursor-pointer bg-red-500 text-white hover:bg-red-500/80 hover:text-white"
+            >
+              Xoá
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default BookingInfoDialog;
--- resources/js/pages/CarBookingManager.tsx
+++ resources/js/pages/CarBookingManager.tsx
@@ -1,14 +1,4 @@
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import { Button } from '@/components/ui/button';
+import BookingInfoDialog from '@/components/BookingInfoDialog';
 import {
   Table,
   TableBody,
@@ -22,9 +12,6 @@ import AppLayout from '@/layouts/app-layout';
 import { BookCarInfo, BreadcrumbItem } from '@/types';
 import formatPhoneNumber from '@/utils/formatPhoneNumber';
 import { Head } from '@inertiajs/react';
-import { format } from 'date-fns';
-import { vi } from 'date-fns/locale';
-import { Info } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
   {
@@ -36,108 +23,11 @@ const breadcrumbs: BreadcrumbItem[] = [
 type CarBookingManagerProps = {
   listBookCar: BookCarInfo[];
 };
-
-const formatLabel = (key: string) => {
-  const labels: { [key: string]: string } = {
-    id: 'ID',
-    ho_ten: 'Họ tên',
-    diem_don: 'Điểm đón',
-    diem_den: 'Điểm đến',
-    check_hai_chieu: 'Hai chiều',
-    check_vat: 'VAT',
-    loai_xe: 'Loại xe',
-    so_dien_thoai: 'Số điện thoại',
-    thoi_gian_don: 'Thời gian đón',
-    thoi_gian_cho: 'Thời gian chờ',
-    created_at: 'Thời gian đặt xe',
-    updated_at: 'Chỉnh sửa lần cuối',
-  };
-  return labels[key] || key;
-};
-const isDateTime = (value: string) => {
-  return !isNaN(Date.parse(value));
-};
-const formatDate = (dateStr: string) => {
-  const date = new Date(dateStr);
-  return format(date, 'dd/MM/yyyy HH:mm', { locale: vi });
-};
-
-const BookingInfoDialog = ({
-  carBookingInfo,
-}: {
-  carBookingInfo: BookCarInfo;
-}) => {
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button variant="outline" className="cursor-pointer">
-          <Info className="sm:hidden" />
-          <p className="hidden sm:block">Xem chi tiết</p>
-        </Button>
-      </AlertDialogTrigger>
-
-      <AlertDialogContent className="max-w-2xl">
-        <AlertDialogHeader>
-          <AlertDialogTitle>Thông tin chi tiết</AlertDialogTitle>
-        </AlertDialogHeader>
-        {/* Bảng hiển thị thông tin */}
-        <div className="overflow-auto">
-          <table className="w-full border-collapse border text-sm">
-            <tbody>
-              {Object.entries(carBookingInfo).map(([key, value]) => (
-                <tr key={key} className="border">
-                  <td className="w-40 bg-gray-100 p-2 text-base font-semibold tracking-wide">
-                    {formatLabel(key)}
-                  </td>
-                  <td className="p-2 text-base tracking-wide break-words hover:bg-gray-100">
-                    {typeof value === 'boolean'
-                      ? value
-                        ? 'Có'
-                        : 'Không'
-                      : typeof value === 'string' && isDateTime(value)
-                        ? formatDate(value)
-                        : String(value)}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <AlertDialogFooter>
-          <AlertDialogCancel className="cursor-pointer">Đóng</AlertDialogCancel>
-          <AlertDialogAction asChild>
-            <Button
-              variant={'outline'}
-              className="cursor-pointer bg-red-500 text-white hover:bg-red-500/80 hover:text-white"
-            >
-              Xoá
-            </Button>
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
-};
-
 const CarBookingManager = ({ listBookCar }: CarBookingManagerProps) => {
-  //
-
   return (
     <AppLayout breadcrumbs={breadcrumbs}>
       <Head title="Dashboard" />
       <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-        {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-          </div>
-          <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-          </div>
-          <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-            <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-          </div>
-        </div> */}
-
         <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min">
           <Table>
             <TableHeader>
